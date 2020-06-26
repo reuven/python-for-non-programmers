6edb4f6e2e82ca1dee250588dbde540f93f63175
Reuven Lerner <reuven@lerner.co.il>
2020-06-26 16:57:39 +0300

    gitautosync update 1

--- testprog.py
+++ testprog.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 def add_one(a_list=[]):
-    """Demonstrate terrible, mutable defaults"""
 
-    a_list.append(1)
-    return a_list
+
+<    """Demonstrate terrible, mutable defaults"""
+
+a_list.append(1)
+return a_list
 
 
 print(add_one())
